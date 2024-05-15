38dcea21c70679c581b02d3540569f9974ff1619
uwihanganye obed <uwihanganyeobed@gmail.com>
2024-05-15 22:25:52 +0200

    all-set

--- index.js
+++ index.js
@@ -11,6 +11,14 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+app.post('/api/fileanalyse', upload.single('upfile'), (req,res)=>{
+  res.json({
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size
+  })
+})
+
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
   console.log('Your app is listening on port ' + port)
